2216bd775c638bb8f9c773596d3fe184a604f1e6
adriancreteanu <creteanu.adrian@gmail.com>
2018-05-13 01:45:43 +0300

    Sprint6_Chat - Customize chat bubble

--- app/pages/ChatPage.js
+++ app/pages/ChatPage.js
@@ -9,7 +9,11 @@ import NavRightIcon from '../components/navigation/NavRightIcon';
 import NavTitleUI from '../components/navigation/NavTitleUI';
 import NavLeftIcon from '../components/navigation/NavLeftIcon';
 
+import * as colors from "../styles/Colors";
+
 import { connect } from 'react-redux';
+
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 
 class ChatPage extends Component {
@@ -38,11 +42,69 @@ class ChatPage extends Component {
         headerTintColor: '#FFF'
     });
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: []
+        }
+    }
+
+    componentWillMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 132231,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 1,
+                        name: 'React Native',
+                    },
+                },
+                {
+                    _id: 121367,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                    },
+                },
+            ],
+        })
+    }
+
+    renderBubble (props) {
+        return (
+          <Bubble
+            {...props}
+            wrapperStyle={{
+              right: {
+                backgroundColor: colors.General.appPrimary
+              }
+            }}
+          />
+        )
+      }
+
+      onSend(messages = []) {
+        this.setState(previousState => ({
+          messages: GiftedChat.append(previousState.messages, messages),
+        }))
+      }
+
     render() {
         return (
-            <View>
-                <Text>Hello</Text>
-            </View>
+            <GiftedChat
+                messages={this.state.messages}
+                renderBubble={this.renderBubble}
+                renderAvatar={null}
+                onSend={messages => this.onSend(messages)}
+                user={{
+                    _id: 1
+                }}
+                
+            />
         )
     }
 }
