14b69b157ff35d69731b698fa1b904f5c3ed4e74
adriancreteanu <creteanu.adrian@gmail.com>
2018-06-01 15:05:04 +0300

    - Install react-native-firebase - Change bundle id name

--- android/app/src/main/java/com/trafficbuddy/MainApplication.java
+++ android/app/src/main/java/com/trafficbuddy/MainApplication.java
@@ -3,6 +3,7 @@ package com.trafficbuddy;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new LinearGradientPackage(),
             new ReactNativeLocalizationPackage(),
             new SplashScreenReactPackage(),
