1852b19f0c0430133ecf1371a0db0efc6ede2e54
adriancreteanu <creteanu.adrian@gmail.com>
2018-06-07 20:56:34 +0300

    Create cloud function for notifications

--- functions/index.js
+++ functions/index.js
@@ -4,25 +4,57 @@ const _ = require('lodash');
 
 admin.initializeApp(functions.config().firebase);
 
+
 exports.sendPushNotification = functions.database
     .ref("/threads/{thread_id}/messages/{message_id}")
     .onWrite(event => {
-
-
         var after = event.after;
-
         let message = after._data.text;
-        let user_id = after._data.user._id;
+        let sender_id = after._data.user._id;
 
-        const payload = {
-            notification: {
-                title: user_id,
-                body: message
+        const getParticipants = after.ref.parent.ref.parent.child("participants").once('value');
+        const getReceiverToken = (receiver_location, receiver_id) => admin.database().ref(`users/${receiver_location}/${receiver_id}/pushToken`).once('value');
+
+        return getParticipants.then(snapshot => {
+
+            // Retrive the participants
+            let participants = snapshot.val();
+            let user1 = _.values(participants)[0];
+            let user2 = _.values(participants)[1];
+
+            // Verify who is the receiver
+            let receiver_id = sender_id === user1 ? String(user2) : String(user1);
+
+            // Calculate user location in database
+            var receiver_location = receiver_id.substring(0, 2);
+
+            // Verification for Bucharest
+            if (!isNaN(receiver_location.charAt(1))) {
+                receiver_location = receiver_id.charAt(0);
             }
-        };
 
-        return admin.messaging()
-            .sendToDevice("fviFOiEV2ow:APA91bFZPjdRO88yGv_pdGhzcvRydSZQeXdWntcMChTS3GZUkVWQ0X0qf_zt1lZLo_Dxmrt4Px3u0RAO2TEun-p0hLjyE39voJq0LuAFzAbLbhnXoheTUh8nHjOPGybKL9nGlB9cAdrS", payload);
+            return Promise.all([snapshot, getReceiverToken(receiver_location, receiver_id)]);
+        })
 
+            .then(results => {
+                let receiver_token = results[1].val();
+
+                // Create the notification payload
+                const payload = {
+                    notification: {
+                        title: sender_id,
+                        body: message,
+                        sound: 'default',
+                    }
+                };
+
+                // Send the notification
+                return admin.messaging()
+                    .sendToDevice(receiver_token, payload);
+            })
+            .catch(error => {
+                console.log(error);
+            })
 
     });
+
